dbed9aae2cd83168cfd1b1fe9e3e9b2525996101
Maria Alexeeva <alxvmr@altlinux.org>
2024-07-24 12:42:55 +0400

    A script is written to update locales

---
+++ update_locales.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+dirs=(locales/*)
+dirs=("${dirs[@]/%//LC_MESSAGES/}")
+source_path="src/"
+
+echo "Enter the name <.po> file: "
+read po_file
+
+echo "Enter the name source (.py): "
+read py_file
+
+for d in "${dirs[@]}"
+do
+    po_path=("${d/%/${po_file}}")
+    mo_path=${po_path:0:${#po_path}-2}"mo"
+    py_path=("${source_path/%/${py_file}}")
+    mess_path="${d/%/"message.po"}"
+
+    xgettext -o "$mess_path" "$py_path"
+    msgmerge -N "$po_path" "$mess_path" > "${d/%/"temp.po"}"
+    mv "${d/%/"temp.po"}" "$po_path"
+    rm "$mess_path"
+
+    msgfmt -o "$mo_path" "$po_path" 
+done
+
+echo "Localization file $po_file updated successfully!"
