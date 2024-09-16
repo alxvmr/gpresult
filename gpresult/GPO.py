d74d85a3d7bed9a16c7a16ab6e7cc50768f35fc3
Maria Alexeeva <alxvmr@altlinux.org>
2024-09-16 11:26:04 +0400

    Preference added to GPO class attributes

--- gpresult/GPO.py
+++ gpresult/GPO.py
@@ -23,23 +23,31 @@ class GPO:
         self.version = kwargs.get("version", None)    # Version
         self.obj = obj                                # Whether the GPO refers to a machine or user object
         self.keys_values = []                         # List of keys and values related to GPOs
+        self.preferences = []                         # Preferences list
 
         GPO.set_gpo(self)
 
 
     def get_info_list(self):
         kvs = self.get_keys_values_lists()
+        prefs = self.get_preferences_lists()
+
         if not kvs:
             kvs = None
+
+        if not prefs:
+            prefs = None
 
         return [
             ["GPO", self.name],
             [_("Path"), self.path],
             [_("Version"), self.version],
             ["GUID", self.guid],
-            [_("Keys and values"), kvs]
+            [_("Keys and values"), kvs],
+            [_("Preferences"), prefs],
+
         ]
-    
+
 
     def get_keys_values_lists(self):
         kvs_list = []
@@ -49,10 +57,19 @@ class GPO:
 
         return kvs_list
 
-    
+
+    def get_preferences_lists(self):
+        prefs_list = []
+
+        for pref in self.preferences:
+            prefs_list.append(pref.get_info_list())
+
+        return prefs_list
+
+
     def get_sorted_keys_values(self):
         return sorted(self.keys_values, key=lambda x: x.key)
-    
+
 
     @classmethod
     def get_all_sorted_keys_values(cls):
@@ -71,9 +88,9 @@ class GPO:
             if gpo.guid == guid:
                 if (obj and gpo.obj == obj) or not obj:
                     gpos_res.append(gpo)
-                
+
         return gpos_res
-    
+
 
     @classmethod
     def get_gpos_by_name(cls, name, obj=None):
@@ -83,19 +100,19 @@ class GPO:
             if gpo.name == name:
                 if (obj and gpo.obj == obj) or not obj:
                     gpos_res.append(gpo)
-                
+
         return gpos_res
 
 
     @classmethod
     def set_keys_values(cls, kv):
         kv_policy_name = kv.policy_name
-        
+
         for gpo in cls.gpos:
             if gpo.name == kv_policy_name and gpo.obj == kv.obj:
                 gpo.keys_values.append(kv)
                 return
-            
+
         return False
 
 
@@ -110,7 +127,7 @@ class GPO:
                 e.name = gpo.name if not e.name else e.name
                 e.version = gpo.version if not e.version else e.version
                 return
-            
+
         cls.gpos.append(gpo)
 
 
@@ -120,3 +137,12 @@ class GPO:
             return cls.gpos
         else:
             return [gpo for gpo in cls.gpos if gpo.obj == obj]
+
+
+    @classmethod
+    def set_preferences(cls, gpo_name, prefs):
+        for gpo in cls.gpos:
+            if gpo.name == gpo_name:
+                for pref in prefs:
+                    if pref.obj == gpo.obj:
+                        gpo.preferences.append(pref)
